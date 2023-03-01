6c40496035fe265bbe73aac0c473c43da1cb8c53
rodrigocns <rodrigoconsoli@gmail.com>
2023-03-01 10:36:00 -0300

    Add MRT task
    Add MRT task to iRT.html
    Add MRT and related files

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 
-//Todas as Funcoes aqui
-const task_list = ["bastao_c","bastao_g","bolaBastao_c","bolaBastao_g","bola_c","bola_g"];
+//Todas as Funcoes da página aqui. Precisa das funcoes referentes aos objetos jmol
+const task_list = ["bastao_c","bastao_g","bolaBastao_c","bolaBastao_g","bola_c","bola_g","mrt"];
 let task_n = 0; //número da tarefa interativa atual
 let stage = 0;
 
@@ -86,7 +86,7 @@ function botaoSubmit(){ //funções para executar com o botão de fim do teste.
   document.getElementById("submitButton").style.visibility="hidden";
   task_n +=1; //Progride para a próxima tarefa em task_list e prepMolecula()
   // if (task_n >= 6) {task_n = 0;} /*volta ao primeiro*/
-  if (task_n >= 6) {
+  if (task_n >= task_list.length) {
     document.getElementById("endTasksButton").style.visibility="visible";
     return;
   } /*finaliza os testes*/
