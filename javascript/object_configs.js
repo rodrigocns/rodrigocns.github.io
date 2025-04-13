a3eedf7b3d7b9017d825683622b06419ad025f37
rodrigocns <rodrigoconsoli@gmail.com>
2025-04-13 18:27:50 -0300

    Update object_configs.js
    Fix, take 3

--- javascript/object_configs.js
+++ javascript/object_configs.js
@@ -7,8 +7,8 @@ var Info_interactive = {
   width: 400,
   use: "HTML5",
   j2sPath: "jsmol/j2s",
-  serverURL: null,
-//  serverURL: "jsmol/php/jsmol.php",
+//  serverURL: null,
+  serverURL: "jsmol/php/jsmol.php",
   disableMouse: true,
 
   defaultModel: "",
